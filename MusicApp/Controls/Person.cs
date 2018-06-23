1773558a5066ef124869d7e5f31d9c4506bded9a
John Kariungi <john.k.gitonga@gmail.com>
2018-06-23 11:49:55 -0700

    Initial commit

---
+++ MusicApp/Controls/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace MusicApp.Controls
+{
+    [DataContract]
+    public class Person// : BindableBase
+    {
+        [DataMember]
+        public string FriendlyName { get; set; }
+
+        public bool IsProfileImage { get; set; }
+
+        public BitmapImage FaceImage { get; set; }
+
+        public Person(string name, string imagePath)
+        {
+
+        }
+
+        private string _imageFileName;
+        [DataMember]
+        public string ImageFileName
+        {
+            get
+            {
+                return _imageFileName;
+            }
+            set
+            {
+                //  SetProperty(ref _imageFileName, value);
+            }
+        }
+    }
+}
---
+++ MusicApp/Utils/MicrosoftPassportHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Credentials;
+
+namespace MusicApp.Utils
+{
+    public static class MicrosoftPassportHelper
+    {
+        public static async Task<bool> MicrosoftPassportAvailableCheckAsync()
+        {
+            bool keyCredentialAvailable = await KeyCredentialManager.IsSupportedAsync();
+            if (keyCredentialAvailable == false)
+            {
+                Debug.WriteLine("Microsoft Passport is not setup!\nPlease go to Windows Settings and set up a PIN to use it.");
+                return false;
+            }
+            return true;
+        }
+    }
+}
